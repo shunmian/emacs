e7cff3f63987591f865700c036cf528cd3d35c13
Johnson <shunmian@gmail.com>
2018-08-17 18:51:16 +0800

    emacs day 2 started

---
+++ init.el
@@ -0,0 +1,117 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+ (when (>= emacs-major-version 24)
+   (require 'package)
+     (package-initialize)
+     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+
+;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+
+ ;; cl - Common Lisp Extension
+ (require 'cl)
+
+ ;; Add Packages
+ (defvar my/packages '(
+                ;; --- Auto-completion ---
+                company
+                smex
+		;; --- Better Editor ---
+                hungry-delete
+                swiper
+                counsel
+                smartparens
+                ;; --- Major Mode ---
+                js2-mode
+                ;; --- Minor Mode ---
+                nodejs-repl
+                exec-path-from-shell
+                ;; --- Themes ---
+                monokai-theme
+                ;; solarized-theme
+                ) "Default packages")
+
+ (setq package-selected-packages my/packages)
+
+ (defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+           when (not (package-installed-p pkg)) do (return nil)
+           finally (return t)))
+
+ (unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+         (package-install pkg))))
+
+ ;; Find Executable Path on OS X
+ (when (memq window-system '(mac ns))
+   (exec-path-from-shell-initialize))
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+					; when Smex is auto-initialized on its first run.
+(global-set-key (kbd "M-x") 'smex)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(electric-indent-mode t)
+(setq inhibit-splash-screen t)
+(global-linum-mode t)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el")
+)
+
+(global-set-key (kbd "<f9>") 'open-my-init-file)
+
+(setq-default cursor-type 'bar)
+
+(require 'recentf)
+(recentf-mode t)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(delete-selection-mode t)
+
+(global-company-mode t)
+(setq make-backup-files nil)
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+
+;- turn off tool-bar
+;- turn off scroll-bar fdsaf
+;- show linum-mode
+;- turn off splash screen
+;- save your config
+;- define a function to quickly open your config file
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+(global-hl-line-mode t)
+
+(load-theme 'monokai t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
